d0b530bdca05c8e4ab9f66c0d5c229e9dd914e16
fatemeshariatisa1360-spec <fateme.shariati.sa1360@gmail.com>
2026-08-08 21:38:50 +0330

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,12 +1,13 @@
 <?php
 define( 'DB_NAME', 'defaultdb' );
 define( 'DB_USER', 'avnadmin' );
-define( 'DB_PASSWORD', 'رمز عبور Aiven خود را اینجا بنویسید' );
+define( 'DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD') );
 define( 'DB_HOST', 'mysql-2db61bda-wpapp2026.aivencloud.com:15360' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
-// تنظیمات امنیتی پیش‌فرض وردپرس (کلیدهای امنیتی را می‌توانید از وب‌سایت وردپرس بگیرید یا همین مقادیر را بگذارید)
+define( 'MYSQL_CLIENT_FLAGS', MYSQL_CLIENT_SSL );
+
 define('AUTH_KEY',         'put your unique phrase here');
 define('SECURE_AUTH_KEY',  'put your unique phrase here');
 define('LOGGED_IN_KEY',    'put your unique phrase here');
